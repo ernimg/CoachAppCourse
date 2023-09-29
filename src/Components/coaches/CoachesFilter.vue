35049bc64cef049a9f8745254f2cba76390ee54b
Ernest <mglernest@gmail.com>
2023-09-29 10:24:42 +0200

    create filter funtion

---
+++ src/Components/coaches/CoachesFilter.vue
@@ -0,0 +1,64 @@
+<template>
+  <h2>Find Your Coach</h2>
+  <span class="filter-option">
+    <input type="checkbox" id="frontend" checked @change="handleInput" />
+    <label for="frontend">Frontend</label>
+  </span>
+  <span class="filter-option">
+    <input type="checkbox" id="backend" checked @change="handleInput" />
+    <label for="backend">Backend</label>
+  </span>
+  <span class="filter-option">
+    <input type="checkbox" id="career" checked @change="handleInput" />
+    <label for="career">Career</label>
+  </span>
+</template>
+<script>
+export default {
+  emits: ['change-filter'],
+  data() {
+    return {
+      filters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
+  },
+  methods: {
+    handleInput(event) {
+      const id = event.target.id;
+      const isChecked = event.target.checked;
+      console.log(id, isChecked);
+      const updateFilters = {
+        ...this.filters,
+        [id]: isChecked,
+      };
+      this.filters = updateFilters;
+      this.$emit('change-filter', updateFilters);
+    },
+  },
+};
+</script>
+<style scoped>
+h2 {
+  margin: 0.5rem 0;
+}
+
+.filter-option {
+  margin-right: 1rem;
+}
+
+.filter-option label,
+.filter-option input {
+  vertical-align: middle;
+}
+
+.filter-option label {
+  margin-left: 0.25rem;
+}
+
+.filter-option.active label {
+  font-weight: bold;
+}
+</style>
--- src/Pages/CoachesList.vue
+++ src/Pages/CoachesList.vue
@@ -1,5 +1,7 @@
 <template>
-  <section>Filter</section>
+  <base-card>
+    <coach-filter @change-filter="setFilter"></coach-filter>
+  </base-card>
   <section>
     <base-card>
       <div class="controls">
@@ -23,16 +25,44 @@
 </template>
 <script>
 import CoachItem from '../Components/coaches/CoachItem.vue';
+import CoachFilter from '../Components/coaches/CoachesFilter.vue';
 export default {
+  data() {
+    return {
+      activeFilter: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
+  },
   components: {
     CoachItem,
+    CoachFilter,
   },
   computed: {
     filterdCoaches() {
-      return this.$store.getters['coachesMod/coaches'];
+      const coaches = this.$store.getters['coachesMod/coaches'];
+      return coaches.filter((coach) => {
+        if (this.activeFilter.frontend && coach.areas.includes('frontend')) {
+          return true;
+        }
+        if (this.activeFilter.backend && coach.areas.includes('backend')) {
+          return true;
+        }
+        if (this.activeFilter.career && coach.areas.includes('career')) {
+          return true;
+        }
+        return false;
+      });
     },
     hasCoaches() {
       return this.$store.getters['coachesMod/hasCoaches'];
+    },
+  },
+  methods: {
+    setFilter(updateFilters) {
+      this.activeFilter = updateFilters;
     },
   },
 };
