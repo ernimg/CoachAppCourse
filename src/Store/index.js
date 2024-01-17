9d72ab0f1bbc0c73e48835b04cc41e95ba5b41a2
ernimg <mglernest@gmail.com>
2024-01-17 21:57:52 +0100

    fix style

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,15 +1,13 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex';
 import Coaches from './modules/coaches/index.js';
-import requestsMod from "./modules/requests/index.js";
+import requestsMod from './modules/requests/index.js';
 import AuthMod from './modules/Auth/index.js';
 const store = createStore({
-    modules: {
-        coachesMod: Coaches,
-        requests: requestsMod,
-        auth: AuthMod
-    },
+  modules: {
+    coachesMod: Coaches,
+    requests: requestsMod,
+    auth: AuthMod,
+  },
+});
 
-})
-
-
-export default store
+export default store;
--- src/Store/modules/Auth/mutations.js
+++ src/Store/modules/Auth/mutations.js
@@ -1,8 +1,8 @@
 export default {
-    setUser(state, payload) {
-        state.token = payload.token;
-        state.userId = payload.userId;
-        state.tokenExpiration = payload.tokenExpiration;
-
-    }
-}
+  setUser(state, payload) {
+    state.token = payload.token;
+    state.userId = payload.userId;
+    state.tokenExpiration = payload.tokenExpiration;
+    localStorage.setItem('token', payload.token);
+  },
+};
